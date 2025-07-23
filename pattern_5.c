eb892db0da97632dbfc7de4e699d5f49a3ff2e6f
Sharanya Hegde <hegdesharanya04@gmail.com>
2025-07-23 23:48:01 +0530

    Add files via upload

---
+++ pattern_5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i==1 || i==n||i==n/2+1)
+        {
+        for(j=1;j<=n;j++)
+            printf("* ");   
+        } 
+        else if(i<n/2+1)
+        {
+            for(j=1;j<=n;j++)
+            {
+                
+                if(j==1)
+                printf("* ");
+                else
+                printf("  ");
+            }
+        }
+        else{
+             for(j=1;j<=n;j++)
+            {
+                
+                if(j==n)
+                printf("* ");
+                else
+                printf("  ");
+            }
+        }
+    printf("\n");
+    }
+    return 0;
+}
